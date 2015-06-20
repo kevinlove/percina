595d9d515ebb1541dc8c2084d9067b7ec11e2fc9
kevinlove <kevinlove@ufl.edu>
2015-06-20 18:09:46 -0400

    added switch for data fields

--- server.R
+++ server.R
@@ -3,38 +3,40 @@ library(ridigbio)
 library(jsonlite)
 
 shinyServer(function(input, output,session) {
-
-
- datasetInput <- reactive({
-  qry <- parseQueryString(session$clientData$url_search)
-  qry <- jsonlite::fromJSON(qry$rq)
-  idig_data <- idig_search_records(rq=qry,fields="all")
-  })
- datasetInputTrim <- reactive({
-  qry <- parseQueryString(session$clientData$url_search)
-  qry <- jsonlite::fromJSON(qry$rq)
-  idig_data <- idig_search_records(rq=qry)
-  })
-  
-  output$table <- renderTable({
-    datasetInputTrim()
-  })
-
-        output$queryText <- renderText({
-            qry <- parseQueryString(session$clientData$url_search)
-            if (length(qry)) {
+        
+        
+        datasetInput <- reactive({
+                qry <- parseQueryString(session$clientData$url_search)
                 qry <- jsonlite::fromJSON(qry$rq)
-                idig_time <- system.time(hol <<- idig_search_records(rq = qry, fields = "all"))
-                if (nrow(hol) > 1) paste("Time taken:", idig_time[1], "\nNumber of records:", nrow(hol),"\nSome data columns have been ommited for easy viewing,","\nuse the download button to view all of the data") else "problem"
-            } else {
-                "You need to specify a query"
-            }
+                idig_data <- idig_search_records(rq=qry,fields="all")
+                idig_data_trim <- idig_search_records(rq=qry)
+                idig_data_id <- idig_search_records(rq=qry,fields=c("catalognumber","collectioncode","institutioncode"))
+                switch(input$dataset,
+                       "Full Data" = idig_data,
+                       "Trimmed Data" = idig_data_trim,
+                       "ID's" = idig_data_id)
         })
-  
-  output$downloadData <- downloadHandler(
-    filename = function() { paste('data', '.csv', sep='') },
-    content = function(file) {
-      write.csv(datasetInput(),row.names=FALSE, file)
-    }
-  )
+        
+        output$table <- renderTable({
+                datasetInput()
+        })
+        
+        output$queryText <- renderText({
+                input$dataset
+                qry <- parseQueryString(session$clientData$url_search)
+                if (length(qry)) {
+                        qry <- jsonlite::fromJSON(qry$rq)
+                        idig_time <- system.time(input$dataset)
+                        if (nrow(hol) > 1) paste(input$dataset, "\nTime taken:", idig_time[1], "\nNumber of records:", nrow(datasetInput())) else "problem"
+                } else {
+                        "You need to specify a query"
+                }
+        })
+        
+        output$downloadData <- downloadHandler(
+                filename = function() { paste(input$dataset, '.csv', sep='') },
+                content = function(file) {
+                        write.csv(datasetInput(),row.names=FALSE, file)
+                }
+        )
 })
--- ui.R
+++ ui.R
@@ -1,12 +1,16 @@
-shinyUI(pageWithSidebar(
-  headerPanel('CSV Download Example'),
-  sidebarPanel(
-    downloadButton('downloadData', 'Download')
-  ),
-  mainPanel(
-    h3("Query"),
-    verbatimTextOutput("queryText"),
-    tableOutput('table')
-
-  )
-))
+shinyUI(
+        navbarPage("iDigBio Download Example",
+                   
+                   ## Download Tab
+                   tabPanel("Download Data",
+                            sidebarPanel(
+                                    selectInput("dataset", "Choose a dataset version:", 
+                                                choices = c("Full Data", "Trimmed Data", "ID's")),
+                                    downloadButton('downloadData','Download')                 
+                            ),
+                            mainPanel(
+                                    h3("Download"),
+                                    verbatimTextOutput("queryText"),
+                                    tableOutput('table')
+                            ))
+        ))
